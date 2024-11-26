ee73e3b1f7604c2e06238729a334bc79d30d193b
AmrTamer23 <amrtamer23mj@gmail.com>
2024-11-26 13:41:47 +0200

    revert

--- fe/src/components/why-choose-us-section.tsx
+++ fe/src/components/why-choose-us-section.tsx
@@ -14,16 +14,16 @@ export default function WhyChooseUs({ content }: WhyChooseUsProps) {
   if (!content) return <div>Loading...</div>;
 
   return (
-    <section className="py-12 lg:py-24 bg-white flex flex-col items-center h-screen gap-8">
+    <section className="py-24 bg-white overflow-hidden flex flex-col items-center h-screen gap-20">
       <div className="flex flex-col gap-4 mx-auto text-center flex-0">
         <span className="uppercase text-gray-500">{content.subtitle}</span>
         <h1 className="text-3xl font-bold">{content.title}</h1>
         <p className="text-lg text-gray-600 max-w-2xl">{content.desc}</p>
       </div>
 
-      <div className="flex justify-between  p-4  lg:max-w-7xl w-full flex-1 flex-col-reverse max-md:gap-8 ">
-        <div className="lg:relative w-full lg:max-w-2xl flex-1 lg:pt-10 sm:flex-col *:mx-auto items-center justify-center flex flex-col-reverse">
-          <div className="lg:absolute lg:left-0 lg:top-0  w-80 h-80 lg:w-96 lg:h-96 bg-[#C05E72] rounded-full flex items-center justify-center  text-white z-10 opacity-85">
+      <div className="flex justify-between  p-4  lg:max-w-7xl w-full flex-1 flex-col gap-8 lg:flex-row">
+        <div className="relative w-full lg:max-w-2xl flex-1 lg:pt-10">
+          <div className="absolute left-0 top-0 w-full lg:w-96  lg:h-96 bg-[#C05E72] rounded-full flex items-center justify-center  text-white z-10 opacity-85">
             <div className="max-w-[200px]">
               <h3 className="font-semibold text-lg mb-2">
                 {content?.Feature?.[activeFeature].Title}
@@ -33,18 +33,18 @@ export default function WhyChooseUs({ content }: WhyChooseUsProps) {
               </p>
             </div>
           </div>
-          <div className="lg:absolute left-80 top-0 w-80 h-80 lg:w-96 lg:h-96 bg-gray-100 rounded-full overflow-hidden">
+          <div className="absolute left-80 top-0 lg:w-96 lg:h-96 bg-gray-100 rounded-full overflow-hidden">
             <img
               src={content?.Feature?.[activeFeature].img?.url ?? ""}
               alt="A person holding a red phone"
               width={1000}
               height={1000}
-              className="object-cover  w-80 lg:w-96"
+              className="object-cover  h-full lg:w-96"
             />
           </div>
         </div>
 
-        <div className="flex flex-col gap-4 ">
+        <div className="flex flex-col gap-4  min-h-full ">
           {content.Feature?.map((feature, index) => (
             <button
               key={index}
@@ -64,7 +64,7 @@ export default function WhyChooseUs({ content }: WhyChooseUsProps) {
                   fill="currentColor"
                   d="m10.828 12l4.95 4.95l-1.414 1.415L8 12l6.364-6.364l1.414 1.414z"
                 />
-              </svg>
+              </svg>{" "}
               <h3 className="text-lg font-semibold">{feature.Title}</h3>
             </button>
           ))}
